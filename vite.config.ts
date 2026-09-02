ad02a0ca4895918159a525038666e44afa4bc4b3
Kevin <kevin.d.fay@gmail.com>
2026-09-01 22:08:31 -0400

    chunk number

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
+    chunkSizeWarningLimit: 400,
     rollupOptions: {
       output: {
         manualChunks: {
